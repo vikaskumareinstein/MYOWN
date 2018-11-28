93b8188f669297f2be83bc1f59ba86d417262f6d
vikaskumareinstein <34993771+vikaskumareinstein@users.noreply.github.com>
2018-11-29 00:48:39 +0530

    Add files via upload

---
+++ Fibon.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+class Fibon
+{
+	public static void main(String args[])
+	{int c=0;int d=0;
+		
+	int a=0,b=1;
+	for(int i=0;;i++)
+	{
+		c=a+b;
+		a=b;
+		b=c;
+		if(c%2==0)
+			d+=c;
+		if(c>=4000000)
+			break;
+	}
+	System.out.println("sum"+d);
+	}
+		
+}
+
+		
+		
+	
+	
+		
+	
